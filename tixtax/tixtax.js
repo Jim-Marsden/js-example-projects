927326c4721560aa8068d19fdb8b79990dcdb26a
Jim-Marsden <jamespmarsden@hotmail.com>
2020-07-16 16:40:33 -0700

    started work on editing elements

--- tixtax/tixtax.js
+++ tixtax/tixtax.js
@@ -1,1 +1,6 @@
-const x = 3;
+const test_element = document.getElementById("p1");
+//test_element.textContent = "test";
+//test_element.innerText = "aaaa";
+
+document.getElementById("p1").innerHTML = "afdasdfasdf";
+test_element.innerHTML = "zztop";
