528045a181ecb5b0c4db57db37f1db5899ed2146
Jim-Marsden <jamespmarsden@hotmail.com>
2020-07-17 02:04:57 -0700

    Interface is mostly done Added tixtax.css

--- tixtax/tixtax.js
+++ tixtax/tixtax.js
@@ -1,6 +1,19 @@
-const test_element = document.getElementById("p1");
-//test_element.textContent = "test";
-//test_element.innerText = "aaaa";
 
-document.getElementById("p1").innerHTML = "afdasdfasdf";
-test_element.innerHTML = "zztop";
+const board = {
+    value: [[null, null, null], [null, null, null], [null, null, null]],
+    setValue: function (top, left, mark) {
+        if (board.value[top][left] == null) {
+            board.value[top][left] = mark;
+            console.log("setting");
+
+        }
+        else console.log("already set!");
+    }
+
+}
+
+function board_buttons_onclick(top, left, mark, obj) {
+    board.setValue(top, left, mark);
+    obj.innerHTML = mark;
+
+}
